a3cf92842adda6113d3d140ece06ce34a143b618
Jose <jose@pcI5-18.(none)>
2012-12-12 09:48:57 +0100

    añadiendo arcivos

---
+++ PGit/src/serpis/psp/HolaMundoGit.java
@@ -0,0 +1,14 @@
+package serpis.psp;
+
+public class HolaMundoGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola desde eclipse-egit");
+
+	}
+
+}
