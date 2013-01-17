0d54639d76e1e6e36a9c0de0867bb296146ac421
Jose <jose@pcI5-18.(none)>
2013-01-17 09:03:11 +0100

    PHttpServer limpio

--- PHttpServer/src/serpis/psp/HttpServer.java
+++ PHttpServer/src/serpis/psp/HttpServer.java
@@ -3,6 +3,7 @@ package serpis.psp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,63 +11,68 @@ import java.util.Scanner;
 
 public class HttpServer {
 
+	private static final String newLine ="\r\n";
 	
 		public static void main (String[] args) throws IOException, InterruptedException{
-			final int port = 8080;
-			String newLine ="\r\n";
-			final String fileNameError404 = "fileError404.html";
-			final String response200 = "HTTP/1.0 200 OK";
-			final String response404 = "HTTP/1.0 404 Not Found";
+			final int port = 8080;		
 			ServerSocket serverSocket = new ServerSocket(port);
 			while(true){
 				Socket socket = serverSocket.accept();
-				Scanner scanner = new Scanner(socket.getInputStream());
 				
-				String fileName="index.html";
-				
-				while(true){
-					String line = scanner.nextLine();
-					System.out.println(line);
-					if (line.equals(""))
-						break;
-				}
-				
-				File file = new File (fileName);
-				
-				String responseFileName = file.exists() ? fileName : fileNameError404;
-				String response = file.exists() ? response200 : response404;
-				
-				String header = response + newLine + newLine;
-				
-				byte[] headerBuffer = header.getBytes();
-				
-				OutputStream outputStream = socket.getOutputStream();
-				outputStream.write(headerBuffer);
-				
-				final int bufferSize = 2048;
-				byte[] buffer = new byte[bufferSize];
-				
-				FileInputStream fileInputStream = new FileInputStream(responseFileName);
-				int count;
-				
-				while ((count = fileInputStream.read(buffer))!=-1){
-					
-					Thread.sleep(5000);
-					outputStream.write(buffer, 0, count);
-				}
-				//System.setProperty("line.separator", "\r\n");
-				
-				/*PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-				
-				printWriter.println("HTTP/1.0 404 Not Found");
-				printWriter.println(newLine);
-				
-				printWriter.flush();*/
-				
-				fileInputStream.close();
+				String fileName= getFileName(socket.getInputStream());	
+				writeHeader(socket.getOutputStream(), fileName);				
+				writeFile(socket.getOutputStream(), fileName);
 				socket.close();
 			}
 			//serverSocket.close();
 		}
+		//TODO implementar correctamente
+		private static String getFileName(InputStream inputStream){
+			
+			Scanner scanner=new Scanner(inputStream);
+			String fileName="index.html";
+			
+			while(true){
+				String line = scanner.nextLine();
+				System.out.println(line);
+				if (line.equals(""))
+					break;
+			}
+			return fileName;
+		}
 
+		private static void writeHeader(OutputStream outputStream, String fileName) throws IOException{
+			final String response200 = "HTTP/1.0 200 OK";
+			final String response404 = "HTTP/1.0 404 Not Found";
+			
+			File file = new File (fileName);
+			String response = file.exists() ? response200 : response404;	
+						
+			String header = response + newLine + newLine;				
+			byte[] headerBuffer = header.getBytes();
+			
+			outputStream.write(headerBuffer);
+		}
+
+		private static void writeFile(OutputStream outputStream, String fileName) throws IOException, InterruptedException{
+			final String fileNameError404 = "fileError404.html";
+			
+			File file = new File (fileName);
+			String responseFileName = file.exists() ? fileName : fileNameError404;				
+			
+			final int bufferSize = 2048;
+			byte[] buffer = new byte[bufferSize];
+			
+			FileInputStream fileInputStream = new FileInputStream(responseFileName);
+			
+			int count;
+			
+			while ((count = fileInputStream.read(buffer))!=-1){
+				
+				Thread.sleep(5000);
+				outputStream.write(buffer, 0, count);
+			}
+			
+			fileInputStream.close();
+		}
 }
